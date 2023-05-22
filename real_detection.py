cebd8922b1481c77911f7aca4b45e52e02e69a31
hqa5im <87118837+hqa5im@users.noreply.github.com>
2023-05-22 20:59:05 +0300

    real_detection.py

--- real_detection.py
+++ real_detection.py
@@ -16,9 +16,10 @@ while(True):
     rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     resized = tf.image.resize(rgb, (150,150))
 
-    yhat = model.predict(np.expand_dims(resized/255,0)) # no clue if this is right
+    yhat = model.predict(np.expand_dims(resized/255,0))
     sample_coords = yhat[1][0]
     
+    # style
     if yhat[0] > 0.5: 
         # Controls the main rectangle
         cv.rectangle(frame, 
